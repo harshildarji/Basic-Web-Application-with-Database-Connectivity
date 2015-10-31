ad3ee2f0befc8285d93fec2fbf3caa41cbe43b27
Harshil <darjiharshil2994@gmail.com>
2015-11-01 00:09:24 +0530

    v 1.1

--- App_Code/Class1.cs
+++ App_Code/Class1.cs
@@ -14,7 +14,7 @@ public class Class1
     SqlConnection con = new SqlConnection();
     public Class1()
     {
-        con.ConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\Harshil\Documents\Visual Studio 2010\WebSites\Fakebook\App_Data\Database.mdf;Integrated Security=True;User Instance=True";
+        con.ConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=D:\VS10 BackUp\WebSites\Fakebook\App_Data\Database.mdf;Integrated Security=True;User Instance=True";
     }
     public void checkcon()
     {
